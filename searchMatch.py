213f372530c09a1688abaee05c489c3b4b00a4f3
Jing Liu <jliu0237@gmail.com>
2023-02-19 19:41:48 -0500

    Update searchMatch.py

--- searchMatch.py
+++ searchMatch.py
@@ -2,21 +2,29 @@ import requests
 from riotwatcher import LolWatcher, ApiError, TftWatcher
 import pandas as pd
 
+lolApiKey = "RGAPI-0175be55-dcd4-4c8d-a300-57d5232f50e6"
+tftApiKey = "RGAPI-00d5ca77-1f7c-4342-b8c0-0cbdebee23c7"
 
+
+# return a dictionary of data or -1 on failure
 def matchSearch(region, game, summonerName):
-    lolApiKey = "RGAPI-0175be55-dcd4-4c8d-a300-57d5232f50e6"
-    tftApiKey = "RGAPI-00d5ca77-1f7c-4342-b8c0-0cbdebee23c7"
+    # identify the game mode searching for
     if game == "lol":
         watcher = LolWatcher(lolApiKey)
-        me = watcher.summoner.by_name(region, summonerName)
+        try:
+            me = watcher.summoner.by_name(region, summonerName)
+        except requests.exceptions.HTTPError as err:
+            print(err)
+            return -1
         print(me)
-
+        # get a list of matches from the summoner
         my_matches = watcher.match.matchlist_by_puuid(region, me['puuid'])
         print(my_matches)
         # fetch last match detail
         last_match = my_matches[0]
         match_detail = watcher.match.by_id(region, last_match)
         print(match_detail['info']['participants'][0].keys())
+        # set dictionaries with useful information for chart display
         participants = []
         for row in match_detail['info']['participants']:
             participants_row = {'champion': row['championId'], 'spell1': row['spell1Casts'],
@@ -29,10 +37,15 @@ def matchSearch(region, game, summonerName):
             participants.append(participants_row)
         df = pd.DataFrame(data=participants)
         print(df.to_string())
+        return df
     elif game == "tft":
         watcher = TftWatcher(tftApiKey)
-        me = watcher.summoner.by_name(region, summonerName)
+        try:
+            me = watcher.summoner.by_name(region, summonerName)
+        except ApiError:
+            return -1
         print(me)
+        # get list of tft matches of the summoner
         my_matches = watcher.match.by_puuid(region, me['puuid'])
         print(my_matches)
         # fetch last match detail
@@ -40,6 +53,7 @@ def matchSearch(region, game, summonerName):
         match_detail = watcher.match.by_id(region, last_match)
         print(match_detail['info']["participants"][0]["traits"])
         print(match_detail['info']['participants'][0].keys())
+        return match_detail
 
 
-matchSearch("na1", "lol", "Llama Smoothie")
+matchSearch("na1", "lol", "llama Smoothie")
